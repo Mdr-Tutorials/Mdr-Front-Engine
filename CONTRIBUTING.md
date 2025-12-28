32ea2590bfaa90eb75044a1c67296f6d5c700ba1
Minsecrus <2972853299@qq.com>
2025-12-28 19:18:46 +0800

    feat(ui): 新增基础组件库并完善文档
    新增文本组件(MdrText, MdrHeading, MdrParagraph)
    新增容器组件(MdrDiv, MdrSection, MdrCard, MdrPanel)
    新增多媒体组件(MdrImage, MdrAvatar, MdrImageGallery, MdrVideo, MdrAudio)
    新增嵌入组件(MdrIframe, MdrEmbed)
    新增输入组件(MdrInput, MdrTextarea, MdrSearch)
    更新组件导出文件index.ts
    完善CONTRIBUTING.md贡献指南
    添加组件样式和交互逻辑

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,3 @@
-# To Contri
+# To Contributors
+
+所有 CSS 类名用大驼峰。组件类名的前缀用 `Mdr`，嵌套的 CSS 类名用完整的单词名称，比如 `.Small`。
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,3 +4,26 @@ export { default as MdrIconLink } from './icon/MdrIconLink';
 export { default as MdrLink } from './link/MdrLink';
 export { default as MdrNav } from './nav/MdrNav';
 export { default as MdrIcon } from './icon/MdrIcon';
+
+export { default as MdrText } from './text/MdrText';
+export { default as MdrHeading } from './text/MdrHeading';
+export { default as MdrParagraph } from './text/MdrParagraph';
+
+export { default as MdrInput } from './input/MdrInput';
+export { default as MdrTextarea } from './input/MdrTextarea';
+export { default as MdrSearch } from './input/MdrSearch';
+
+export { default as MdrDiv } from './container/MdrDiv';
+export { default as MdrSection } from './container/MdrSection';
+export { default as MdrCard } from './container/MdrCard';
+export { default as MdrPanel } from './container/MdrPanel';
+
+export { default as MdrImage } from './image/MdrImage';
+export { default as MdrAvatar } from './image/MdrAvatar';
+export { default as MdrImageGallery } from './image/MdrImageGallery';
+
+export { default as MdrVideo } from './video/MdrVideo';
+export { default as MdrAudio } from './video/MdrAudio';
+
+export { default as MdrIframe } from './embed/MdrIframe';
+export { default as MdrEmbed } from './embed/MdrEmbed';
